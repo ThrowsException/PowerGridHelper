e50f750b766349f474d94a6bdc2aa5033d237b0e
Chester ONeill <c.oneill.codes@gmail.com>
2013-11-12 00:55:55 -0500

    got v0.0.1 working

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,10 @@
-"use strict";
+'use strict';
 
 var coal = [];
 var oil = [];
 var trash = [];
 var uranium = [];
+var price = 0;
 
 function initializeResources() {
 
@@ -62,22 +63,72 @@ function initializeResources() {
 
 }
 
+function createEventHandler(resourceType, resourceArray, i) {
+  var d = document.getElementById(resourceType + '_' + i);
+  d.addEventListener('click', function() { resourceClicked(resourceType, resourceArray, resourceArray[i].available); }, false );
+}
+
 function createLayout(resourceType, resourceArray, element) {
   var el = document.getElementById(element);
   var parent = el.parentNode;
-  for(var i = 0; i < resourceArray.length; i++) {
+  var i = resourceArray.length - 1;
+  
+  for(; i >= 0; i--) {
     var d = document.createElement('span');
-    d.className += resourceType + '_display';
+    d.className = resourceType + '_display';
+    d.id = resourceType + '_' + i;
+    
     parent.insertBefore(d, el.nextSibling);
   }
+
+  i = resourceArray.length - 1;
+  for(; i >= 0; i--) {
+    createEventHandler(resourceType, resourceArray, i);
+  }
+}
+
+function resourceClicked(resourceType, resourceArray, available) {
+  var done = false;
+  var resource, block, i = 0, max = 0;
+  var total = document.getElementById('total');
+  if(available) {
+    max = resourceArray.length;
+
+    for(i = 0; i < max && !done; i++) {
+      resource = resourceArray[i];
+      if(resource.available === available) {
+        done = true;
+        resource.available = !available;
+        block = document.getElementById(resourceType + '_' + i);
+        block.innerHTML = 'X';
+      }
+    }
+
+    price += resource.price;
+  }
+
+  if(!available) {
+    for(i = resourceArray.length - 1; i >= 0 && !done; i--) {
+      resource = resourceArray[i];
+      if(resource.available === available) {
+        done = true;
+        resource.available = !available;
+        block = document.getElementById(resourceType + '_' + i);
+        block.innerHTML = '';
+      }
+    }
+
+    price -= resource.price;
+  }
+  total.innerHTML = '$' + price;
 }
 
 (function() {
   
   initializeResources();
 
-  createLayout("coal", coal, "coal_label");
-  createLayout("oil", oil, "oil_label");
-  createLayout("trash", trash, "trash_label");
-  createLayout("uranium", uranium, "uranium_label");
+  createLayout('coal', coal, 'coal_label');
+  createLayout('oil', oil, 'oil_label');
+  createLayout('trash', trash, 'trash_label');
+  createLayout('uranium', uranium, 'uranium_label');
 }());
